6a64d5fa451201ca91a0743a6b28a9351f110467
Teresa Tavernelli <teresa.tavernelli@gmail.com>
2020-10-02 23:37:53 -0400

    Cambios pequeños en tecnicargo

--- index.php
+++ index.php
@@ -13,6 +13,17 @@
     $elcorreo .= "$mensaje \n";
 
     mail($destinatario, $asunto, $elcorreo);
+
+    if (@mail($destinatario, $asunto ,$elcorreo )) {
+            //Si el mensaje se envía muestra una confirmación
+            die("Gracias, su mensaje se envio correctamente.");
+            header("Location: http://www.tutecnicargo.com");
+        }else{
+            //Si el mensaje no se envía muestra el mensaje de error
+            die("Error: Su información no pudo ser enviada, intente más tarde");
+        }
+        
+        exit();
     
 
 ?>
